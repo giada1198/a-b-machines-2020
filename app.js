267b10806e6b0565539804641bbfb94f23d12434
Giada Sun <giada1198@gmail.com>
2020-09-17 20:31:15 -0700

    init

---
+++ app.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDGz-JZAC6I61gl7fVJ_pcvR9BE2hO7V90",
+    authDomain: "ab-machines-2020.firebaseapp.com",
+    databaseURL: "https://ab-machines-2020.firebaseio.com",
+    projectId: "ab-machines-2020",
+    storageBucket: "ab-machines-2020.appspot.com",
+    messagingSenderId: "265193914832",
+    appId: "1:265193914832:web:2add09353e2b72da6039d5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
