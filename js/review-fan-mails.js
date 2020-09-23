cf64032b0c7d653a2fe98174a183dc05339295c7
Giada Sun <giada1198@gmail.com>
2020-09-23 16:08:32 -0700

    v3

--- js/review-fan-mails.js
+++ js/review-fan-mails.js
@@ -12,19 +12,19 @@ appId: "1:265193914832:web:2add09353e2b72da6039d5"
 firebase.initializeApp(firebaseConfig);
 let ref = firebase.database().ref('fan-mails').orderByChild('time');
 
-let content = document.getElementById('content');
+let container = document.getElementById('mails');
 
 ref.once('value', function(mails) {
 	mails.forEach(function(mail) {
 		let val = mail.val();
-		content.innerHTML += ('<div class="mail">' +
-							  '<img src="' + val['image'] + ' width="200" height="200">' +
-							  '<br><b>No:</b> ' + mail.key +
-							  '<br><b>To:</b> ' + val['to'] +
-							  '<br><b>Name:</b> ' + val['name'] +
-							  '<br><b>From:</b> ' + val['location'] +
-							  '<br><b>Message:</b> ' + val['message'] +
-							  '<br><b>Posted:</b> ' + val['time'] +
-							  '</div>');
+		container.innerHTML += ('<div class="mail">' +
+								'<img src="' + val['image'] + ' width="200" height="200">' +
+								'<p><b>No:</b> ' + mail.key +
+								'<br><b>To:</b> ' + val['to'] +
+								'<br><b>Name:</b> ' + val['name'] +
+								'<br><b>From:</b> ' + val['location'] +
+								'<br><b>Message:</b> ' + val['message'] +
+								'<br><b>Posted:</b> ' + val['time'] + '</p>' +
+								'</div>');
 	});
 });
